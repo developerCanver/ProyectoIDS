7bfaf0a464392c42940698cd8d741a8416411925
developerCanver <canver199@gmail.com>
2021-04-17 22:59:41 -0500

    prueba linux 2 descarga

--- resources/views/descargar.blade copy.php
+++ resources/views/descargar.blade copy.php
@@ -12,38 +12,40 @@
 
         <div class="row">
            @php
-    
+               //****************************paquetes adicionales paa ubuntu --------------- */
+        //shell_exec('apt-get update');
+        //shell_exec('apt-get upgrade');
+        //shell_exec('sudo apt-get dist-upgrade -y');
+        //primeras sentencias de codigo
         
-            echo("Iniciando Descarga Snort <br>");
-            //echo('instalando automake <br><br>');
-            //print_r(shell_exec('sudo apt-get install -y automake'));
-            //echo(' <br><br> instalando autorecof <br><br>');
-            //print_r(shell_exec('sudo apt-get install -y dh-autoreconf'));
+            //echo("Iniciando Descarga Snort <br>");
+            echo('instalando automake <br><br>');
+            print_r(shell_exec('sudo apt-get install -y automake'));
+            echo(' <br><br> instalando autorecof <br><br>');
+            print_r(shell_exec('sudo apt-get install -y dh-autoreconf'));
 
             $daq= shell_exec('sh bash/daq.sh');
 
            
-             if ($daq == "Descargando") {
-                echo('Descargando primer paquete...');
+             /*if ($daq == "Descargando") {
                 exec('tar xvzf daq-2.0.7.tar.gz');
              }else{
                 exec('wget https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz');
                 exec('tar xvzf daq-2.0.7.tar.gz');
                 
                 $daq= 'Daq descargado sactifactoriamente';
-             }
+             }*/
 
              $snort = shell_exec('sh bash/snort.sh');
 
              
-             if ($snort == "Descargando") {
-               echo('Descargando Segundo paquete...');
+             /*if ($snort == "Descargando") {
                 exec('tar xvzf snort-2.9.17.tar.gz');
              }else{
-                exec('wget https://www.snort.org/downloads/snort/snort-2.9.17.1.tar.gz');
+                exec('wget https://www.snort.org/downloads/snort/snort-2.9.17.tar.gz');
                 exec('tar xvzf snort-2.9.17.tar.gz');
                 $snort= 'Snort descargado sactifactoriamente';
-             }
+             }*/
 
 
            @endphp
